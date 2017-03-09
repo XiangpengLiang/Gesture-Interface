c2cc40556b67f6c1b40dd4dae0ac344536439685
Derek <100251618@qq.com>
2017-03-09 00:31:31 +0000

    Voltage Measurement
    MCP3008

--- SPI.c
+++ SPI.c
@@ -42,15 +42,28 @@ int main(int argc, char **argv)
     // If you tie MISO to MOSI, you should read back what was sent
 
 while(1){
-    uint8_t send_data = 0x80;
-    uint8_t read_data = bcm2835_spi_transfer(send_data);
+    uint8_t send_data[2] = {0xC0,0x00};
+    uint8_t read_data[3];
 
+    bcm2835_spi_transfernb(send_data,read_data,3);
 
-    printf("Sent to SPI: 0x%02X. Read back from SPI: 0x%02X.\n", send_data, read_data);
+    printf("Sent to SPI: 0x%02X. Read back from SPI: 0x%02X.  0x%02X.  0x%02X \n", send_data[0], read_data[0],read_data[1],read_data[2]);
+ 
+uint8_t a,b,c;
+uint16_t v;
+float voltage;
 
-    if (send_data != read_data)
-      printf("Do you have the loopback from MOSI to MISO connected?\n");
+a= ((read_data[0]<<1) + (read_data[1]>>7));
+b= (((read_data[1]<<1) + (read_data[2]>>7)))&0x7F;
+c= read_data[2]>>7;
+v= ((a<<8)|(b<<1)|c);
+voltage= v*3.3/1023;
 
+printf("a= 0x%02X. b= 0x%02X.  c= 0x%02X. \n", a,b,c);
+
+printf("voltage = %fV \n", voltage);
+      printf("\n\n");
+bcm2835_delay(1000);
 }
     bcm2835_spi_end();
     bcm2835_close();
